deefc7ab9b7da0def5018c53e27792889858ac87
Amr Mohamad <amr.mohamed124124@gmail.com>
2022-08-21 16:28:22 +0000

    fix formatting result

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 print("Welcome to Tip Calculator.")
 totalBill = float(input("What was the total bill? $"))
 percentageOfTip = float(input("What percentage tip would you like to give? 10%, 12%, 15% or more than 15% ==>").replace("%",""))
-print(percentageOfTip)
+#print(percentageOfTip)
 numberOfPeople = float(input("How many people to split the bill? "))
 
 tipValue = totalBill * (percentageOfTip / 100)
 totalPayedValue = totalBill + tipValue
-eachPersonShouldPay = round(totalPayedValue / numberOfPeople , 3)
+eachPersonShouldPay = round(totalPayedValue / numberOfPeople , 2)
+eachPersonShouldPay = "{:.2f}".format(eachPersonShouldPay)
 print(f"Each Person should pay: ${eachPersonShouldPay}")
